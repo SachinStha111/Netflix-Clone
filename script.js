0a98654afb06776f760b8c109782de2a954bbbc1
Sachin Shrestha <sachinstha809@gmail.com>
2025-05-23 08:10:18 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function LogIn(){
+    alert("Thanks a lot! Your support means everything to me.")
+}
+
+function SignIn(){
+    alert("May each day bring you kindness, strength, and beautiful moments to cherish.")
+}
